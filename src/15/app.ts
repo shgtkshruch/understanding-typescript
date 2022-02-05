f7dccafe56244c671a742a43999d581f9224d4b9
shgtkshruch <shgtk.shruch@gmail.com>
2022-02-05 12:50:08 +0900

    Add lesson 15

---
+++ src/15/app.ts
@@ -0,0 +1,16 @@
+(() => {
+  // const person: {
+  //   name: string;
+  //   age: number;
+  // } = {
+  //   name: "yota",
+  //   age: 30,
+  // };
+
+  const person = {
+    name: "yota",
+    age: 30,
+  };
+
+  console.log(person.name);
+})();
